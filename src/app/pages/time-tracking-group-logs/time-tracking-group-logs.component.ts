3ec57d071217bc6b7c4d0cfc4e25d95a2e481a5d
Anna Andrzejewska-Godecka <46266571+RhiannonAnna@users.noreply.github.com>
2020-01-20 21:16:45 +0100

    Base Architectura (#1)

---
+++ src/app/pages/time-tracking-group-logs/time-tracking-group-logs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-tracking-group-logs',
+  templateUrl: './time-tracking-group-logs.component.html',
+  styleUrls: ['./time-tracking-group-logs.component.scss']
+})
+export class TimeTrackingGroupLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/time-tracking-groups/time-tracking-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-tracking-groups',
+  templateUrl: './time-tracking-groups.component.html',
+  styleUrls: ['./time-tracking-groups.component.scss']
+})
+export class TimeTrackingGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
